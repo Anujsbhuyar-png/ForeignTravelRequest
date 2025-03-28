82737765aff61cc9487d52e36f66f1bae98b257b
Aadarsh Pravin Wadile <aadarsh.pravinwadile.external@eviden.com>
2025-03-28 11:48:25 +0530

    Added the UI Part(Controller and View for the same) of the CreateFtr and Documnet Upload page

---
+++ FTR.UI/Controllers/CreateFtr.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FTR.UI.Controllers
+{
+    public class CreateFtr : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
